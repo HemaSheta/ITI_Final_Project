7b286469ac448d16f3c948911af2af21572fa747
Heba Nagy <hebanagy3333@gmail.com>
2025-08-19 17:58:08 +0300

    Finish Course Management

--- Project_Final_ITI/Migrations/20250819145717_CourseNameUnique.Designer.cs
+++ Project_Final_ITI/Migrations/20250819145717_CourseNameUnique.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project_Final_ITI.Data;
 
@@ -11,9 +12,11 @@ using Project_Final_ITI.Data;
 namespace Training_Managment_System.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250819145717_CourseNameUnique")]
+    partial class CourseNameUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
