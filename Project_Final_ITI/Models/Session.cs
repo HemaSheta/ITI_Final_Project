4d44951e07a397f81decb2dd25940ddf908b6059
Heba Nagy <hebanagy3333@gmail.com>
2025-08-20 10:57:45 +0300

    Edit Session Model and Add a Migration

--- Project_Final_ITI/Models/Session.cs
+++ Project_Final_ITI/Models/Session.cs
@@ -8,17 +8,19 @@ namespace Project_Final_ITI.Models
     {
         public int SessionId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Start date is required")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "End date is required")]
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Course selection is required")]
         public int CourseId { get; set; }
 
         public Course Course { get; set; }
-        public ICollection<Grade> Grades { get; set; } // Trainee grades for this session
+        public ICollection<Grade> Grades { get; set; } = new List<Grade>(); // Trainee grades for this session
 
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
