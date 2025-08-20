652a09fa165a4246c4c50ec14d3183e6c0087dc2
mahmoud salah <mahmoud.mahmed.salah84@gmail.com>
2025-08-20 18:46:12 +0300

    00

--- Project_Final_ITI/Repositories/Implementations/GradeRepository.cs
+++ Project_Final_ITI/Repositories/Implementations/GradeRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Training_Managment_System.Repositories.Implementations
+﻿using Project_Final_ITI.Data;
+using Project_Final_ITI.Models;
+using Training_Managment_System.Repositories.Interfaces;
+
+namespace Training_Managment_System.Repositories.Implementations
 {
-    public class GradeRepository
+    public class GradeRepository :  BaseRepository<Grade>, IGradeRepository
     {
+        public GradeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
     }
 }
