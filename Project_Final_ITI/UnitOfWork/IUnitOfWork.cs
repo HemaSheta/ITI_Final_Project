90eab624b0047d4e1c65b46f72c78df8e00cd74c
Heba Nagy <hebanagy3333@gmail.com>
2025-08-20 11:00:05 +0300

    Add Unit Of Work of Session By Eng Fares

--- Project_Final_ITI/UnitOfWork/IUnitOfWork.cs
+++ Project_Final_ITI/UnitOfWork/IUnitOfWork.cs
@@ -5,6 +5,10 @@ namespace Training_Managment_System.UnitOfWork
     public interface IUnitOfWork
     {
         ICourseRepository CourseRepository { get; }
+        ISessionRepository SessionRepository { get; }
+
+        // saving changes
+        Task<int> SaveAsync();
 
     }
 }
--- Project_Final_ITI/UnitOfWork/UnitOfWork.cs
+++ Project_Final_ITI/UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,24 @@
-﻿namespace Training_Managment_System.UnitOfWork
+﻿using Project_Final_ITI.Data;
+using Training_Managment_System.Repositories.Interfaces;
+
+namespace Training_Managment_System.UnitOfWork
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext cont;
+
+        public ICourseRepository CourseRepository { get; }
+        public ISessionRepository SessionRepository { get; }
+
+        public UnitOfWork(ApplicationDbContext context,
+                          ICourseRepository courseRepository,
+                          ISessionRepository sessionRepository)
+        {
+            cont = context;
+            CourseRepository = courseRepository;
+            SessionRepository = sessionRepository;
+        }
+
+        public Task<int> SaveAsync() => cont.SaveChangesAsync();
     }
 }
